c969d044fd8c59503669d7a361e73912e7c017a3
ErickDArx <alexalonso2706@gmail.com>
2021-05-16 21:20:37 -0600

    NPM

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,17 +8,15 @@
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('../resources/css/app.css') }}">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
         crossorigin="anonymous" />
-    <link rel="stylesheet" href="{{ asset('../resources/css/flickity.css') }}" media="screen">    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
-    </script>
-    <script src="{{ asset('../resources/js/flickity.pkgd.min.js') }}"></script>
+    <link rel="stylesheet" href="https://unpkg.com/flickity-fullscreen@1/fullscreen.css">
+    <link rel="stylesheet" href="{{ asset('../resources/css/flickity.css') }}" media="screen">
+    <link rel="stylesheet" href="{{ asset('../resources/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('../resources/css/center-simple.css') }}">
 </head>
 
 <body class="">
@@ -45,17 +43,12 @@
     </nav>
 
     <div>
-
+        <div class="carousel" data-flickity='{ "imagesLoaded": true, "percentPosition": false,"autoPlay": true, "fullscreen": true, "wrapAround": true, "lazyLoad": 1 }'>
         @foreach ($files as $item)
-
-        <div class="carousel-cell container img-fluid">
-            <img width="100%" src="{{$item->url}}" alt="">
-        </div>
-
+            <img src="{{$item->url}}" alt="">
         @endforeach
-
+        </div>
     </div>
-
 
     {{-- Administrador --}}
     <div
@@ -67,6 +60,39 @@
         </form>
     </div>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+</script>
 
+<script src="{{ asset('../resources/js/flickity.pkgd.min.js') }}"></script>
+<script src="https://unpkg.com/flickity-fullscreen@1/fullscreen.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+<script>
+    var elem = document.querySelector('.main-carousel');
+    var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+    });
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity('.main-carousel', {
+    // options
+});
+
+var flkty = $('.carousel').flickity({
+    fullscreen: true,
+});
+
+paceOptions = {
+  // Disable the 'elements' source
+  elements: false,
+
+  // Only show the progress on regular and ajax-y page navigation,
+  // not every request
+  restartOnRequestAfter: false
+}
+</script>
 
 </html>
